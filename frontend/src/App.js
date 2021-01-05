ca7eccb8a185d4c22f739aec3403283c0eaa9518
Farhan Ghazi <farhansghazi@outlook.com>
2021-01-05 17:36:01 +1100

    set up react boilerplate using react-cra

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Big things soon come.</h1>
+    </div>
+  );
+}
+
+export default App;
